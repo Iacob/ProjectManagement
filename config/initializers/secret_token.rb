350bd9b0b88956ea513fceed816b4dd4844d109d
Luo Yong <luo.yong.name@gmail.com>
2011-05-31 21:38:25 +0800

    Initial upload.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManagement::Application.config.secret_token = 'eeacceca205efeb665fd98874b2f6bd7babfc2da62370c08fe44d9146436716ee5caff580c57619af5b079448dc843c5c2e2a97569c858e250a1f545d5cdbc20'
