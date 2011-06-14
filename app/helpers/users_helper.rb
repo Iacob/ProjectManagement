8a4b4516a6b24958ff52bfeb68d90af008acf00c
Iacob <luo.yong.name@gmail.com>
2011-06-14 00:44:01 -0700

    Add image link view helper. Add user statistics module.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def sayHello
-    "Hello from user."
+  def icon_link(icon, target, width, height)
+    raw("<a href=\"#{target}\"><img src=\"#{icon}\" width=\"#{width}\" height=\"#{height}\"/><a/>")
   end
 end
